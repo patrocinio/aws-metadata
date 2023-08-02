ce5064199383a227ec7693881cd1dd5a69adfbcb
Eduardo Patrocinio <epatro@amazon.com>
2023-08-02 08:43:26 -0400

    Use ECS_CONTAINER_METADATA_URI_V4

--- metadata.js
+++ metadata.js
@@ -1,26 +1,24 @@
 import got from 'got'
 
-const GET_URL = "http://169.254.170.2/v2/metadata"
-
-export async function run (event, context, callback) {
-  context.callbackWaitsForEmptyEventLoop = false
-
-	console.log ("Running...");
-	console.log ("URL: " + GET_URL)
-
-  try {
-    const response = await got(RESET_URL)
-    console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
-    console.log("response: ", response)
-    return response
-  } catch (e) {
-    console.log ("Error getting metadata " + e)
-  }
-}
+const GET_URL = process.env.ECS_CONTAINER_METADATA_URI_V4;
 
 export class metadata {
+  async run () {
+    console.log ("Running...");
+    console.log ("URL: " + GET_URL)
+  
+    try {
+      const response = await got(GET_URL)
+      console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
+      console.log("response: ", response)
+      return response
+    } catch (e) {
+      console.log ("Error getting metadata " + e)
+    }
+  }
+
   async get (res) {
-    const response = await run()
+    const response = await this.run()
     res.send (response)
   }
 }
